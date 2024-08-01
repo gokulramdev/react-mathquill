93520eb41ef914475caaf28b55c1928153987c33
Gokulram <66685828+gokulramdev@users.noreply.github.com>
2024-08-01 10:30:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import style from 'mathquill/build/mathquill.css'
+import style from '../build/mathquill.css'
 
 export function addStyles() {
   if (document.getElementById('react-mathquill-styles') == null) {
